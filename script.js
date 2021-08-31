c17fc00650a0fd6a50c8c43a524a2af4fdbe55b6
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2021-08-31 23:16:15 +0530

    made changes with the board logic

--- script.js
+++ script.js
@@ -1,58 +1,121 @@
-const difficulty={
-    easy:["00","02","03","05","06","07","12","14","16","18","21","22","25","35","37",
-    "40","41","44","45","48","55","56","57","58","60","61","62","65","66","67","68","70","71","74","77","85","86","88"],
-    medium:["04","05","07,08","13","16","17","18","24","27","28","30","35","37","38","41","43","44","46","48","50","60","61","65","67","68","70","74","82","83"],
-    hard:[00,01,03,14,16,17,18,23,24,26,30,32,33,37,51,54,55,56,57,60,62,64,72,73,77,86,87,88]
+const difficulties={
+    easy:[
+        [0,3,0,0,6,5,2,4,0],//0
+        [0,0,0,0,0,0,0,0,0],//1
+        [0,0,8,2,3,0,0,0,0],//2
+        [0,0,0,0,0,6,1,0,0],//3
+        [0,8,9,0,0,0,0,5,0],//4
+        [0,0,7,0,8,9,0,0,0],//5
+        [4,9,0,1,0,0,0,0,2],//6
+        [7,0,0,0,5,8,0,6,9],//7
+        [0,0,0,0,0,0,4,0,0]//8
+    ],
+    medium:[
+        [0,0,0,0,6,0,0,7,0],//0
+        [0,0,8,7,4,0,0,5,0],//1
+        [0,0,0,0,0,0,0,0,0],//2
+        [0,7,0,8,0,0,0,0,0],//3
+        [2,0,0,0,3,0,6,4,0],//4
+        [0,4,0,0,0,9,0,3,1],//5
+        [0,5,0,3,0,0,2,0,0],//6
+        [0,0,0,0,9,0,0,0,0],//7
+        [0,1,7,0,0,0,5,0,0]//8
+    ] ,
+    hard:[
+        [0,6,0,9,1,0,0,0,0],//0
+        [5,0,7,0,0,0,0,0,8],//1
+        [0,2,0,0,0,0,0,5,0],//2
+        [0,0,0,7,0,9,6,0,4],//3
+        [0,0,6,8,0,0,0,0,7],//4
+        [0,4,0,0,0,0,0,3,0],//5
+        [0,0,0,0,0,4,5,1,0],//6
+        [4,0,0,0,0,6,7,0,9],//7
+        [0,0,0,0,0,7,0,0,0]//8
+    ]    
 }
 const values={
-    easy:[4,9,8,2,1,7,1,4,3,9,3,5,6,3,9,6,9,5,7,3,4,6,2,7,9,2,6,8,4,5,1,1,5,2,3,1,9,2],
-    medium:[7,2,1,3,9,6,8,7,3,5,4,9,7,4,5,5,3,9,7,6,4,2,1,6,3,8,6,2,4,5],
-    hard:[]
+    easy:[3,6,5,2,4,8,2,3,6,1,8,9,5,7,8,9,4,9,1,2,7,5,8,6,9,4],
+    medium:[6,7,8,7,4,5,7,8,2,3,6,4,4,9,3,1,5,3,2,9,1,7,5],
+    hard:[6,9,1,5,7,8,2,5,7,9,6,4,6,8,7,4,3,4,5,1,4,6,7,9,7]
 }
 let easy=document.querySelector("#easy");
 let medium=document.querySelector("#medium");
 let hard=document.querySelector("#hard");
-easy.addEventListener("click",()=>{
-    for(var z=0;z<difficulty.easy.length;z++){
-        for(var i=0;i<9;i++){
-            for(var j=0;j<9;j++){
-                if((""+i+j)===difficulty.easy[z]){
-                   
-                    var ele=document.getElementById(`c${i}${j}`);
-                    ele.disabled=true;
-                    ele.style.background="red";
-                    console.log(values.easy[z])
-                    ele.value=values.easy[z];
 
-                }
-               
-               
-            }
+ window.onload=()=>{
+    easy.focus();
+    for(let a=0;a<9;a++){
+    for(let b=0;b<9;b++){
+        if(difficulties.easy[a][b]!==0){
+         var ele=document.getElementById(`c${a}${b}`);
+         ele.disabled=true;
+         ele.style.background="#EBEBE4";
+         ele.value=difficulties.easy[a][b];
         }
-    }
+    } 
+ }   
+ }
+   
+
+const clearPreviousBoard=()=>{
+  for(let i=0;i<9;i++){
+      for(let j=0;j<9;j++){
+          var clearEle=document.getElementById(`c${i}${j}`).value;
+          
+          if(clearEle!=="")
+          {    
+              var removeThese=document.getElementById(`c${i}${j}`);
+              
+              removeThese.disabled=false;
+              removeThese.value="";
+              removeThese.style.background="#fff";
+          }
+      }
+  }
     
+}
+easy.addEventListener("click",()=>{
+
+    clearPreviousBoard();
+    for(let a=0;a<9;a++){
+       for(let b=0;b<9;b++){
+           if(difficulties.easy[a][b]!==0){
+            var ele=document.getElementById(`c${a}${b}`);
+            ele.disabled=true;
+            ele.style.background="#EBEBE4";
+            ele.value=difficulties.easy[a][b];
+           }
+       } 
+    }
 })
 
 medium.addEventListener("click",()=>{
-    for(var z=0;z<difficulty.medium.length;z++){
-        for(var i=0;i<9;i++){
-            for(var j=0;j<9;j++){
-                if((""+i+j)===difficulty.medium[z]){
-                   
-                    var ele=document.getElementById(`c${i}${j}`);
-                    ele.disabled=true;
-                    ele.style.background="red";
-                    console.log(values.medium[z])
-                    ele.value=values.medium[z];
 
-                }
-               
-               
+    clearPreviousBoard();
+
+    for(let a=0;a<9;a++){
+        for(let b=0;b<9;b++){
+            if(difficulties.medium[a][b]!==0){
+             var ele=document.getElementById(`c${a}${b}`);
+             ele.disabled=true;
+             ele.style.background="#EBEBE4";
+             ele.value=difficulties.medium[a][b];
             }
-        }
-    }
+        } 
+     }
 })
 
 hard.addEventListener("click",()=>{
-    console.log(difficulty.hard.length)
+    clearPreviousBoard();
+
+    for(let a=0;a<9;a++){
+        for(let b=0;b<9;b++){
+            if(difficulties.hard[a][b]!==0){
+             var ele=document.getElementById(`c${a}${b}`);
+             ele.disabled=true;
+             ele.style.background="#EBEBE4";
+             ele.value=difficulties.hard[a][b];
+            }
+        } 
+     }
 })
